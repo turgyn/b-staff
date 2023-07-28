bec9993486fc760c7e61cb72e9b2aa697d72acb8
darkhan <darustur@gmail.com>
2023-07-29 00:00:23 +0600

    rename to bereke-staff

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
-docker build -t komek-backend .
+docker build -t bereke-staff .
 
-docker run -p 8080:8080 komek-backend
+docker run -p 8080:8080 bereke-staff
 
 docker run --name my-postgres -e POSTGRES_PASSWORD=turgyn -e POSTGRES_USER=turgyn -d -p 5432:5432 postgres
--- src/main/java/kz/berekebank/BStaffApplication.java
+++ src/main/java/kz/berekebank/BStaffApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KomekBackendApplication {
+public class BStaffApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KomekBackendApplication.class, args);
+        SpringApplication.run(BStaffApplication.class, args);
     }
 
 }
